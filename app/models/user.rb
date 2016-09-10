0e19571f0a1799bd078fa225e97d780dbb3cd49e
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-09-10 16:11:41 -0700

    Update user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	validates :name, presence: true, uniqueness: true
-	# has_secure_password
-	# validates :password, presence: true, length: { minimum: 6 }
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
 end
