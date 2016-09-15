8083230c2e1817900ecf26313de51007c73c2a27
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-09-15 14:14:56 -0700

    Update puma.rb from Heroku Puma documentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,11 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-		@user.save
-		
-		redirect_to @user
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
 	end
 
 	private
