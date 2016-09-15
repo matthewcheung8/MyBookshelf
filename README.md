c11459e89aa36384daab8d2b8bd7c0d197895ef0
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-09-15 13:51:23 -0700

    Update README

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
-#MyBookshelf
+# MyBookshelf
 
-###What this is?
+### What this is for?
 Rails web application that allows users to share books they have enjoyed to other people. Post, comment, like away!
 
-###What I learned:
+### What I learned:
 - Routes
 - REST principles
 - Heroku deployment
 - Bcrypt gem for authentication
 - Secure Sockets Layer for information protection over networks
+
+## Planning the app:
+
+### User Stories
+	- As a user, I want to be able to update, edit, and destroy book posts so that I can share books that I have enjoyed or disliked with others.
+	- As a user, I want to be able to comment and like other users' book posts so that I can express my opinions on their book preferences.
+
+### Model our Data
+	1. Book
+		title:string
+		description:text
+	2. User
+		name:string	
+		password:string
+		password_confirmation:string
+	3. Comment
+
+### Pages
+		- Home
+		- Books#index
+		- Books#show
+		- User#signin
+		- User#show
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,41 +1,47 @@
 class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@books = Book.all.order('created_at DESC')
 	end
 
-	def show
-		@book = Book.find(params[:id])
-	end
-
 	def new
+		@book = Book.new
 	end
 
 	def create
 		@book = Book.new(book_params)
-		@book.save
+		if @book.save
+			redirect_to @book
+		else
+			render 'new'
+		end
+	end
 
-		redirect_to @book
+	def show
 	end
 
 	def edit
-		@book = Book.find(params[:id])
 	end
 
 	def update
-		@book = Book.find(params[:id])
-		@book.update(book_params)
-		
-		redirect_to @book
+		if @book.update(book_params)
+			redirect_to @book
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-		@book = Book.find(params[:id])
 		@book.destroy
-
 		redirect_to books_path
 	end
 
 	private
+		def find_book
+			@book = Book.find(params[:id])
+		end
+
 		def book_params
 			params.require(:book).permit(:title, :author, :description, :likes)
 		end
